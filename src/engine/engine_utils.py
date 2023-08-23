f8edd6b8aed8bfe06afa6de5d1f54087379eb339
XInTheDark <muzhengaming@gmail.com>
2023-08-23 13:35:21 +0800

    Cleanup engine_utils.py

--- src/engine/engine_utils.py
+++ src/engine/engine_utils.py
@@ -1,46 +1,7 @@
 import chess, chess.engine
 import datetime
-import yaml
 
-ROOT_BOARD = None
-
-PGN_TEXT = f"""[Event "PVplayer analysis"]
-[Site "https://github.com/XInTheDark/PVplayer"]
-[Date "{datetime.datetime.now().strftime('%Y.%m.%d')}"]
-"""
-MOVE_COUNT = 1
-
-export_pgn = detailed_pgn = False
-
-
-def __init__():
-    global export_pgn, detailed_pgn
-    
-    with open("config.yml", "r") as f:
-        config = yaml.safe_load(f)
-        export_pgn = config["EXPORT_PGN"]
-        detailed_pgn = config["DETAILED_PGN"]
-
-
-def push_pv(start, pv, info=None, is_tb=False):
-    global PGN_TEXT, MOVE_COUNT, ROOT_BOARD
-    
-    board = None
-    if type(start) == str:
-        board = chess.Board(start)
-    elif type(start) == chess.Board:
-        board = start
-    
-    # Initialise PGN_TEXT if this is the start
-    if export_pgn and not "FEN" in PGN_TEXT:
-        PGN_TEXT += f"[FEN \"{board.fen()}\"]\n"
-    if ROOT_BOARD is None:
-        ROOT_BOARD = chess.Board(board.fen())
-    
-    # Add a note if this is a tablebase PV
-    if is_tb:
-        PGN_TEXT += "{ Start of tablebase PV } "
-    
+def push_pv(board: chess.Board, pv, info=None, is_tb=False):        
     if type(pv) == str:
         pv = pv.split('pv')[-1]
         pv = pv.split()
@@ -49,69 +10,13 @@ def push_pv(start, pv, info=None, is_tb=False):
             move = chess.Move.from_uci(move)
     
     rule50 = board.halfmove_clock
-    for move in pv:
-        if export_pgn:
-            if board.turn == chess.WHITE:
-                PGN_TEXT += f"{MOVE_COUNT}. {board.san(move)} "
-            else:
-                PGN_TEXT += f"{board.san(move)} "
-                MOVE_COUNT += 1
-        
+    for move in pv:        
         board.push(move)
         
         if board.is_game_over(claim_draw=True):
             break
-        
-        rule50 += 1
-        
-        # Reset rule50 count because the library implementation is somewhat broken
-        if board.is_capture(move):
-            rule50 = board.halfmove_clock = 0
-        elif board.piece_at(move.from_square).piece_type == chess.PAWN:
-            rule50 = board.halfmove_clock = 0
     
-    # Append info at end of the current PV iteration in the pgn
-    if export_pgn and detailed_pgn and info is not None:
-        PGN_TEXT += f"{{ {info['score']}, depth {info['depth']}/{info['seldepth']}," \
-                    f" {info['nodes']} nodes }} "
-    
-    # Correct the rule50 count in FEN
-    f = board.fen().split()
-    f[-2] = str(rule50)
-    f = ' '.join(f)
-    
-    ROOT_BOARD = board
-    
-    return chess.Board(f)
-
-
-def write_pgn():
-    """Write the saved PGN into the /PVplayer/pgns directory (create it if it doesn't exist)"""
-    global PGN_TEXT
-    
-    if not export_pgn:
-        return
-    
-    print("Writing PGN...")
-    
-    # Create the directory if it doesn't exist
-    import os
-    os.chdir("..")
-    if not os.path.exists("pgns"):
-        os.mkdir("pgns")
-    
-    # Write the PGN
-    FILE_NAME = f"pgns/{datetime.datetime.now().strftime('%Y.%m.%d %H.%M.%S')}.pgn"
-    with open(FILE_NAME, "w") as f:
-        f.write(PGN_TEXT)
-    
-    # Reset PGN_TEXT
-    PGN_TEXT = f"""[Event "PVplayer analysis"]
-    [Site "https://github.com/XInTheDark/PVplayer"]
-    [Date "{datetime.datetime.now().strftime('%Y.%m.%d')}"]
-    """
-    
-    print(f"PGN written to {FILE_NAME}")
+    return board
 
 
 def cp_to_score(cp):
