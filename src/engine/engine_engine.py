4961444d6c86e5357751153ca37a0206050f1c47
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-10-08 22:53:47 +0800

    Update: auto download stockfish

--- src/engine/engine_engine.py
+++ src/engine/engine_engine.py
@@ -4,6 +4,8 @@ import chess.engine
 from engine_ucioption import *
 from engine_timeman import Time
 
+import os
+
 engine: chess.engine.SimpleEngine = None
 engine_options = {
     "Threads": option("Threads"),
@@ -16,10 +18,30 @@ def init_engine():
         engine.quit()
     
     ENGINE_PATH = option("ENGINE_PATH")
+    ENGINE_MISSING = False
     try:
         engine = chess.engine.SimpleEngine.popen_uci(ENGINE_PATH)
     except FileNotFoundError:
-        return
+        ENGINE_MISSING = True
+    
+    try:
+        engine.analyse(chess.Board(), chess.engine.Limit(nodes=1))
+    except Exception:
+        ENGINE_MISSING = True
+    
+    if ENGINE_MISSING:
+        print("Engine (specified in ENGINE_PATH option) not found! Attempting to download Stockfish.")
+        # Download engine
+        from engine_download import download_stockfish
+        try:
+            ENGINE_PATH = download_stockfish()
+        except Exception as e:
+            print("Failed to download engine. Error:", e)
+            os._exit(0)
+            
+        options["ENGINE_PATH"] = ENGINE_PATH
+        engine = chess.engine.SimpleEngine.popen_uci(ENGINE_PATH)
+        print("Engine downloaded successfully.")
     
     setoptions_engine()
 
